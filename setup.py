ea678e994dc1c5000b7482f8855a15ea0f0bcfc7
Marty McGuire <schmartissimo@gmail.com>
2017-02-14 13:03:40 -0500

    bump version for oops

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-IndieAuth',
-    version='0.0.3',
+    version='0.0.3.1',
     url='https://github.com/martymcguire/flask-indieauth/',
     license='MIT',
     author='Marty McGuire',
